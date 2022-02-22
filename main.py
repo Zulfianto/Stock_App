10ef088a99aa3e146fb08642f786c91dd63d2dec
Zulfianto <68130540+Zulfianto@users.noreply.github.com>
2022-02-23 02:40:23 +0700

    Update main.py

--- main.py
+++ main.py
@@ -312,7 +312,7 @@ def candlestick(input_value):
         margin=dict(t=30, l=0, r=0, b=0),
         xaxis=dict(range=[df.index[-80], df.index[-1] + timedelta(minutes=6)], rangeslider_visible=False,
                    rangebreaks=[
-                       dict(bounds=["sat", "tue"]),
+                       dict(bounds=["sat", "mon"]),
                        dict(bounds=[16, 9.5], pattern="hour"),
                        #dict(values=["2022-02-21", ])
                    ]),
@@ -564,9 +564,9 @@ def candlestick1(input_value):
         plot_bgcolor='white',
         height=670,
         margin=dict(t=30, l=0, r=0, b=0),
-        xaxis=dict(range=[df.index[-60], df.index[-1] + timedelta(minutes=20)], rangeslider_visible=False,
+        xaxis=dict(range=[df.index[-60], df.index[-1] + timedelta(minutes=22)], rangeslider_visible=False,
                    rangebreaks=[
-                       dict(bounds=["sat", "tue"]),
+                       dict(bounds=["sat", "mon"]),
                        dict(bounds=[16, 9.5], pattern="hour"),
                        #dict(values=["2022-02-21", ])
                    ]),
